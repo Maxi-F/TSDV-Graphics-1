448c707ebe56bfee47cd3788fb0cb6719ef82a33
Maxi Feldman <maximiliano.a.feldman@gmail.com>
2024-08-27 12:16:19 -0300

    Add test shader

--- TSDV-Graphics-1/src/Renderer.cpp
+++ TSDV-Graphics-1/src/Renderer.cpp
@@ -1,5 +1,19 @@
 #include "Renderer.h"
+#include "iostream"
 
+const char* vertexShaderSource = "#version 330 core\n"
+"layout (location = 0) in vec3 aPos;\n"
+"void main()\n"
+"{\n"
+"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+"}\0";
+
+const char* fragmentShaderSource = "#version 330 core\n"
+"out vec4 FragColor;\n"
+"void main()\n"
+"{\n"
+"	FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+"}\0";
 
 int GuichernoEngine::Renderer::vertexCount = 0;
 float GuichernoEngine::Renderer::vertices[];
@@ -17,6 +31,66 @@ void GuichernoEngine::Renderer::SwapBuffers(Window window)
 void GuichernoEngine::Renderer::GenerateBuffer()
 {
 	glewInit();
+
+	unsigned int vertexShader;
+	vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+	glCompileShader(vertexShader);
+
+	int success;
+	char infoLog[512];
+	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+
+	if (!success)
+	{
+		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+	}
+	else {
+		std::cout << "Compile vertex success..." << std::endl;
+	}
+
+	unsigned int fragmentShader;
+	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+	glCompileShader(fragmentShader);
+
+	int fragmentSuccess;
+	char fragmentInfoLog[512];
+	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &fragmentSuccess);
+
+	if (!fragmentSuccess)
+	{
+		glGetShaderInfoLog(fragmentShader, 512, NULL, fragmentInfoLog);
+		std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << fragmentInfoLog << std::endl;
+	} else {
+		std::cout << "Compile fragment success..." << std::endl;
+	}
+
+
+	unsigned int shaderProgram;
+	shaderProgram = glCreateProgram();
+
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	glLinkProgram(shaderProgram);
+
+	int linkSuccess;
+	char linkInfoLog[512];
+	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &linkSuccess);
+
+	if (!linkSuccess)
+	{
+		glGetProgramInfoLog(shaderProgram, 512, NULL, linkInfoLog);
+		std::cout << "ERROR::PROGRAM::LINK_FAILED\n" << linkInfoLog << std::endl;
+	} else {
+		std::cout << "Link shaders success..." << std::endl;
+	}
+
+	glUseProgram(shaderProgram);
+
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
 
 	float* verticesToBuffer = new float[vertexCount];
 
@@ -37,8 +111,11 @@ void GuichernoEngine::Renderer::GenerateBuffer()
 	// Set data on the buffer.
 	glBufferData(GL_ARRAY_BUFFER, sizeof(float) * vertexCount, verticesToBuffer, GL_STATIC_DRAW);
 
+	// Create the layout for the vertices
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
+
+	std::cout << "Set Everything" << std::endl;
 
 	delete[] verticesToBuffer;
 }
