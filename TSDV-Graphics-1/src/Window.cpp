313f6c893b198e051c506b1efb06b0e14c5de381
Maxi-F <maximiliano.a.feldman@gmail.com>
2024-09-13 20:47:47 -0300

    Add projection to window

--- TSDV-Graphics-1/src/Window.cpp
+++ TSDV-Graphics-1/src/Window.cpp
@@ -1,13 +1,24 @@
 #include "Window.h"
 
+GuichernoEngine::Window::Window() {
+	this->gmWindow = NULL;
+	this->projection = NULL;
+}
+
 GuichernoEngine::Window::Window(int width, int heigth, const char* windowName)
 {
 	this->gmWindow = glfwCreateWindow(width, heigth, windowName, NULL, NULL);
+	this->projection = glm::ortho(0.0f, (float)width, 0.0f, (float)heigth, -1.0f, 1.0f);
 }
 
 GLFWwindow* GuichernoEngine::Window::GetGLFWwindow()
 {
 	return gmWindow;
+}
+
+glm::mat4 GuichernoEngine::Window::GetProjection()
+{
+	return projection;
 }
 
 void GuichernoEngine::Window::MakeCurrent()
