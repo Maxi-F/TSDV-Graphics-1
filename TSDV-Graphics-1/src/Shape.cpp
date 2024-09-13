e835f78c1692a5d177659751a400af5bcb112395
Maxi-F <maximiliano.a.feldman@gmail.com>
2024-09-13 20:48:18 -0300

    Add Model to Shape & Update

--- TSDV-Graphics-1/src/Shape.cpp
+++ TSDV-Graphics-1/src/Shape.cpp
@@ -20,5 +20,16 @@ void GuichernoEngine::Shape::Draw()
 {
 	Renderer renderer;
 
-	renderer.DrawElements(this->bufferData);
+	renderer.DrawElements(this->bufferData, this->model);
+}
+
+void GuichernoEngine::Shape::Update()
+{
+	// rotate the model once per second
+	float angle = 0.0f;
+	float speed = 0.5f;
+
+	angle += speed * 1.0f;
+
+	this->model = glm::rotate(this->model, glm::radians(angle), glm::vec3(0.0f, 0.0f, 1.0f));
 }
