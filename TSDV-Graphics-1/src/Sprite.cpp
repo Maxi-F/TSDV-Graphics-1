f222ff3d8cd69f123eae6cf5bdf62daf6f1f2507
Maxi Feldman <maximiliano.a.feldman@gmail.com>
2024-10-15 10:15:49 -0300

    Create Texture Importer

--- TSDV-Graphics-1/src/Sprite.cpp
+++ TSDV-Graphics-1/src/Sprite.cpp
@@ -1,9 +1,7 @@
 #include "Sprite.h"
 #include "ShaderType.h"
 #include "Renderer.h"
-#define STB_IMAGE_IMPLEMENTATION
-#include "stb_image.h"
-#include <iostream>
+#include "TextureImporter.h"
 
 const unsigned int SQUARE_VERTEX_LENGTH = 9;
 const unsigned int SQUARE_ARRAY_LENGTH = 36;
@@ -33,27 +31,7 @@ GuichernoEngine::Sprite* GuichernoEngine::Sprite::FromRectangle(const char* file
 GuichernoEngine::Sprite::Sprite(const char* filePath, float* vertices, unsigned int vertexLength, unsigned int arrayLength, ShapeType shapeType) :
 	Entity2D(vertices, vertexLength, arrayLength, shapeType)
 {
-	/* TODO use texture importer */
-	glGenTextures(1, &this->texture);
-	glBindTexture(GL_TEXTURE_2D, this->texture);
-	// set the texture wrapping/filtering options (on the currently bound texture object)
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-	// load and generate the texture
-	int width, height, nrChannels;
-	unsigned char* data = stbi_load(filePath, &width, &height, &nrChannels, 0);
-	if (data)
-	{
-		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
-		glGenerateMipmap(GL_TEXTURE_2D);
-	}
-	else
-	{
-		std::cout << "Failed to load texture" << std::endl;
-	}
-	stbi_image_free(data);
+	TextureImporter::Import(filePath, this->texture);
 }
 
 GuichernoEngine::Sprite::~Sprite()
---
+++ TSDV-Graphics-1/src/TextureImporter.h
@@ -0,0 +1,8 @@
+#pragma once
+
+namespace GuichernoEngine {
+	class TextureImporter {
+		public:
+			static void Import(const char* fileName, unsigned int &texture);
+	};
+}
