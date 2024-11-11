4023bd78c06a42c122a24840711d7ac5655bb108
Maxi-F <maximiliano.a.feldman@gmail.com>
2024-11-11 15:14:04 -0300

    Set rgb or rgba type by channels number

--- TSDV-Graphics-1/src/TextureImporter.cpp
+++ TSDV-Graphics-1/src/TextureImporter.cpp
@@ -8,7 +8,7 @@
 void GuichernoEngine::TextureImporter::Import(const char* fileName, TextureData &textureData) {
 	glGenTextures(1, &textureData.ID);
 	glBindTexture(GL_TEXTURE_2D, textureData.ID);
-	// set the texture wrapping/filtering options (on the currently bound texture object)
+
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
@@ -16,15 +16,28 @@ void GuichernoEngine::TextureImporter::Import(const char* fileName, TextureData 
 	
 	std::string fileNameString = std::string(fileName);
 	std::string extension = fileNameString.substr(fileNameString.find_last_of(".") + 1);
-	bool isRgba = extension == "png";
 
 	// load and generate the texture
 	int nrChannels;
 	stbi_set_flip_vertically_on_load(true);
-	unsigned char* data = stbi_load(fileName, &textureData.width, &textureData.height, &nrChannels, isRgba ? STBI_rgb_alpha : STBI_rgb);
+	unsigned char* data = stbi_load(fileName, &textureData.width, &textureData.height, &nrChannels, 0);
+
+	GLenum format;
+	
+	switch (nrChannels) {
+		case 3:
+			format = GL_RGB;
+			break;
+		case 4:
+			format = GL_RGBA;
+			break;
+		default:
+			format = GL_RGB;
+	}
+
 	if (data)
 	{
-		glTexImage2D(GL_TEXTURE_2D, 0, isRgba ? GL_RGBA : GL_RGB, textureData.width, textureData.height, 0, isRgba ? GL_RGBA : GL_RGB, GL_UNSIGNED_BYTE, data);
+		glTexImage2D(GL_TEXTURE_2D, 0, format, textureData.width, textureData.height, 0, format, GL_UNSIGNED_BYTE, data);
 		glGenerateMipmap(GL_TEXTURE_2D);
 	}
 	else
